bff859a7206b2d0b95836675d68753198659b332
omarnader456 <omansour101@outlook.com>
2025-04-15 19:23:48 +0200

    first commit

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:myapp/main.dart' as app;
-
+//end to end automated testing
 void main() {
       IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
